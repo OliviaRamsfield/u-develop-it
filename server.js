7a75e6787b8c3f9df815ac51f16ab007b7b7b48c
Olivia <ramsfol@gmail.com>
2022-08-09 12:14:08 -0500

    connected to the database

--- server.js
+++ server.js
@@ -2,10 +2,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to SQL database
+const db = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        //mysql  username
+        user: 'root',
+        //mysql password
+        password: 'auri',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response for any other request (NOT FOUND) - must come after all other routes
 app.use((req, res) => {
